e4f43aca9981c4472f227551c6992f147608441d
Danish Nisar <danishnisar786@gmail.com>
2023-09-04 01:32:58 +0500

    added splat route

---
+++ app/routes/$.jsx
@@ -0,0 +1,15 @@
+import { Response } from "@remix-run/node";
+import { redirect } from "react-router"
+import { json } from "stream/consumers";
+
+
+
+export function loader({params}){
+    
+    console.log(params);
+    if(params['*'] === 'exp'){
+        return redirect('/expenses');
+    }
+    return new Response('404 page not found',{status:404})
+   
+}
