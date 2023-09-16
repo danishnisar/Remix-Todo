bfee4a5d884c6da6ab26aa77c7a16757adcfc644
Tiffany Chu <remix-run@dev.com>
2023-09-16 10:59:31 -0700

    test autodeployment

--- app/component/util/Logo.jsx
+++ app/component/util/Logo.jsx
@@ -1,11 +1,11 @@
 import { Link } from "@remix-run/react";
-import {FaMastodon } from 'react-icons/fa';
+import {FaMastodon,FaMagento } from 'react-icons/fa';
 
 function Logo() {
   return (
     <h1 id="logo">
       
-      <Link href="/"><FaMastodon /> RemixExpenses</Link>
+      <Link href="/"><FaMastodon /> RemixExpenses <FaMagento/> </Link>
     </h1>
   );
 }
